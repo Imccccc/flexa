a841658b2ca5e528ca4da4469a7ae7f63e1f8b64
ccccc <changjing@mobike.com>
2017-10-29 17:21:37 +0800

    update

--- include/mri.h
+++ include/mri.h
@@ -10,7 +10,7 @@
 			int problem_solver;
 			int	rank, size;
 			double lambda;
-			int Kx, Ky, t, n, Ky_local, n_local, nnz, nnz_local;
+			int Kx, Ky, t, n, nnz;
 			double *y, *y_zf, *y_big, *x, *b, *F, *nablaF;
 			int *mask; double *image, *reference_image; 
 			DFTI_DESCRIPTOR_HANDLE dft_descriptor_handle_Kx; MKL_LONG status_Kx;
@@ -30,17 +30,11 @@
 			void apply_mask(double *input, double *output);
 			void zero_filling(double *input, double *output);
 			void solve_subproblem_ST_J(double rho, double M_global, double gamma, double *delta, int update_strategy, double *local_function_value);
-			void solve_subproblem_ST_GS(double rho, double M_global, double tau, double gamma, double *delta, int update_strategy, double *local_function_value);
 		public:
 			// Methods
 			MRI(int rank, int size, std::string data_directory, double lambda, bool verbose, int problem_solver);
 			~MRI();
-			void solve_flexa(double merit_tolerance, int max_iterations, double max_time, int solver, int update_strategy, double tau, double gamma, double rho);
-			void solve_fista(double merit_tolerance, int max_iterations, double max_time, double power_method_tolerance, int power_method_max_iterations);
-			void solve_sparsa(double merit_tolerance, int max_iterations, double max_time, double eta, double rho, int M);
-			void solve_grock(double merit_tolerance, int max_iterations, double max_time, int mode, int P);
-			void solve_ktfocuss(double merit_tolerance, int max_iterations, double max_time, int inner_loop_limit, int downsampling_factor, double rho);
-			void solve_barista(double merit_tolerance, int max_iterations, double max_time);			
+			void solve_flexa(double merit_tolerance, int max_iterations, double max_time, int solver, int update_strategy, double tau, double gamma, double rho);		
 			void save_results();
 	
 		};
